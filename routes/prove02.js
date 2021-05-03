76e0c2d8b8aac6d41bc50c488e750430a35ce2d9
mkarkee <ermkarki@gmail.com>
2021-05-02 21:36:56 -0600

    prove01

---
+++ routes/prove02.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+const books = [];
+function loadJSON(filename = ''){
+    if(fs.existsSync(filename)) {
+        return JSON.parse(fs.readFileSync(filename).toString());
+    }
+    else {
+        return JSON.parse('""');
+    }
+}
+
+function saveJSON(filename, json = '""'){
+    return fs.writeFileSync(filename, JSON.stringify(json ,null, 2));
+}
+
+router.get('/', (req, res, next) => {
+    res.render('pages/prove02', {
+        title: 'Prove02 - Assignment',
+        path: '/prove02'
+    });
+});
+
+
+router.post('/display-book', (req, res, next) => {
+    const book = JSON.stringify(req.body, null, 2);
+    const data = loadJSON('books.json');
+    
+    data.push(req.body);
+    saveJSON('books.json', data)
+    
+    res.render('pages/display-book', {
+        title: req.body.book_title,
+        summary: req.body.summary,
+        path: '/display-book'
+        
+    });
+});
+
+module.exports = router;
